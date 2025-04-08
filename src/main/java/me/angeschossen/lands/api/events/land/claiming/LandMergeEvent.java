7cc5d8ff3f7398293ade0ae9dd5d6d0b6d010950
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2025-04-08 12:31:33 +0200

    api doc

--- src/main/java/me/angeschossen/lands/api/events/land/claiming/LandMergeEvent.java
+++ src/main/java/me/angeschossen/lands/api/events/land/claiming/LandMergeEvent.java
@@ -7,6 +7,9 @@ import me.angeschossen.lands.api.player.LandPlayer;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Called when a land owner accepts a merge request.
+ */
 public class LandMergeEvent extends LandCancellableEvent {
     public static HandlerList handlerList = new HandlerList();
     private final @NotNull Land toMerge;
@@ -14,7 +17,7 @@ public class LandMergeEvent extends LandCancellableEvent {
     /**
      * Constructor for this event.
      *
-     * @param requester       involved land
+     * @param requester  involved land
      * @param landPlayer involved player
      */
     public LandMergeEvent(@NotNull Land requester, @NotNull Land toMerge, LandPlayer landPlayer) {
