9e81d7bbe5fef598ff27ba42517c23bd227f3bfd
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2025-11-07 23:20:14 +0100

    7.19.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ description = "LandsAPI"
 
 plugins {
     `java-library`
-    id("io.github.goooler.shadow").version("8.1.8")
+    id("com.gradleup.shadow").version("9.2.2")
     `maven-publish`
 }
 
