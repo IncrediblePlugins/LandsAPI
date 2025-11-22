9847e16cdab057c490f029c0a5d23d6aece4761e
Angeschossen <24841122+Angeschossen@users.noreply.github.com>
2025-11-22 21:30:53 +0100

    doc capture area

--- src/main/java/me/angeschossen/lands/api/war/captureflag/CaptureArea.java
+++ src/main/java/me/angeschossen/lands/api/war/captureflag/CaptureArea.java
@@ -3,7 +3,19 @@ package me.angeschossen.lands.api.war.captureflag;
 import me.angeschossen.lands.api.land.LandWorld;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * A capture area can for example be a sub area of a land or a collection if chunks.
+ */
 public interface CaptureArea {
 
+    /**
+     * Checks if the location is inside the capture area.
+     *
+     * @param world the world
+     * @param x     block X
+     * @param y     block Y
+     * @param z     block Z
+     * @return true, if the location is inside the capture area
+     */
     boolean contains(@NotNull LandWorld world, int x, int y, int z);
 }
