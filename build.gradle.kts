594dabc85f198e0f3e24df59c396ff85cac74925
Artichoke-Dev <artichoke-dev@proton.me>
2026-03-29 00:11:40 +0100

    update build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.github.incredibleplugins.base") version "1.0.18-SNAPSHOT"
+    id("com.github.incredibleplugins.base") version "1.0.23-SNAPSHOT"
 }
 
 group = "com.github.angeschossen"
@@ -34,7 +34,7 @@ tasks {
 }
 
 dependencies {
-    shadow("com.github.angeschossen:pluginframework-api:1.1.43")
+    shadow("com.github.angeschossen:pluginframework-api:1.1.47")
 
     compileOnly("org.realityforge.org.jetbrains.annotations:org.jetbrains.annotations")
     compileOnly("org.spigotmc:spigot-api:1.21.1-R0.1-SNAPSHOT")
